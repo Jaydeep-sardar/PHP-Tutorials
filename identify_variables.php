8b277188146dea3618b4f7003c76de030d157016
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-13 21:03:04 +0530

    variable identification in PHP

---
+++ identify_variables.php
@@ -0,0 +1,11 @@
+<?php
+// Identifying variables in PHP
+$name = "John";
+$age = 25;
+$isStudent = true;
+
+// Output variables
+echo "Name: $name\n";
+echo "Age: $age\n";
+echo "Is Student: " . ($isStudent ? "Yes" : "No") . "\n";
+?>

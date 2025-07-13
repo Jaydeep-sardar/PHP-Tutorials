f3f87dcb8f6b9bbf4c9a1d1ad24af36c6823b992
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-13 21:05:20 +0530

    constants definition and output in PHP

---
+++ constants.php
@@ -0,0 +1,10 @@
+<?php
+// Constants in PHP
+
+define("SITE_NAME", "MyWebsite");
+const PI = 3.14159;
+
+// Output constants
+echo "Site Name: " . SITE_NAME . "\n";
+echo "Value of PI: " . PI . "\n";
+?>

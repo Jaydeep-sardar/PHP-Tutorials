9be48ed5994933d79a32ef086477ee89a0820213
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-13 21:03:46 +0530

    PHP and HTML integration

---
+++ php_html_together.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP and HTML Together</title>
+</head>
+<body>
+    <h1>Welcome!</h1>
+    <p>
+        <?php
+        $name = "Alice";
+        echo "Hello, $name!";
+        ?>
+    </p>
+</body>
+</html>

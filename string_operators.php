39c1535e5de68c2b346f7a5c9aa9eab859683042
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-13 21:09:19 +0530

    Add string operators demonstration in PHP

---
+++ string_operators.php
@@ -0,0 +1,14 @@
+<?php
+// String Operators in PHP
+
+$str1 = "Hello";
+$str2 = "World";
+
+// Concatenation operator
+$result = $str1 . " " . $str2;
+echo "Concatenation: $result\n";
+
+// Concatenation assignment operator
+$str1 .= " PHP";
+echo "Concatenation Assignment: $str1\n";
+?>

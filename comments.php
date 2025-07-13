8cef3f1c1e3f2903dabfaa3122dcc5edb70da258
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-13 21:06:29 +0530

    comments and example code in PHP

---
+++ comments.php
@@ -0,0 +1,15 @@
+<?php
+// Comments in PHP
+
+// This is a single-line comment
+# This is also a single-line comment
+/*
+   This is a multi-line comment
+   It can span multiple lines
+*/
+
+// Example code with comments
+$greeting = "Hello, World!"; // Assign a string to variable
+
+echo $greeting; // Output the greeting
+?>

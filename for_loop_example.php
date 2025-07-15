ea7450b22901132d56bf247257feb240ea72f46d
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-15 20:44:38 +0530

    Add for loop and even number printing examples in PHP

---
+++ for_loop_example.php
@@ -0,0 +1,14 @@
+<?php
+// For loop in PHP
+
+for ($i = 1; $i <= 5; $i++) {
+    echo "Number: $i\n";
+}
+
+// Example: Print even numbers from 1 to 10
+for ($j = 1; $j <= 10; $j++) {
+    if ($j % 2 == 0) {
+        echo "$j is even\n";
+    }
+}
+?>

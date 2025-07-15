bc3c1e53c06ea8d8cdd8fe4bd5e8d5e8fb9901ab
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-15 20:43:30 +0530

    Add elseif condition demonstration in PHP

---
+++ elseif_example.php
@@ -0,0 +1,15 @@
+<?php
+// Elseif condition in PHP
+
+$marks = 75;
+
+if ($marks >= 90) {
+    echo "Grade: A\n";
+} elseif ($marks >= 75) {
+    echo "Grade: B\n";
+} elseif ($marks >= 60) {
+    echo "Grade: C\n";
+} else {
+    echo "Grade: D\n";
+}
+?>

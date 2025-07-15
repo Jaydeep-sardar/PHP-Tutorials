3d18a43833394465b5730cc258e9fd52db5a18cb
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-15 20:46:25 +0530

    Add initial PHP functions for addition and greeting

---
+++ function_example.php
@@ -0,0 +1,19 @@
+<?php
+// Function in PHP
+
+// Simple function to add two numbers
+function add($a, $b) {
+    return $a + $b;
+}
+
+// Calling the function
+$result = add(5, 7);
+echo "Sum of 5 and 7 is: $result\n";
+
+// Function to display a greeting
+function greet($name) {
+    echo "Hello, $name!\n";
+}
+
+greet("John");
+?>

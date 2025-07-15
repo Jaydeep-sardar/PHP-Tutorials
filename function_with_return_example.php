65bd581bd75ad7535f5820aac293e405e5281710
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-15 20:47:21 +0530

    Add function with return statement example in PHP

---
+++ function_with_return_example.php
@@ -0,0 +1,10 @@
+<?php
+// Function with return statement in PHP
+
+function multiply($a, $b) {
+    return $a * $b;
+}
+
+$result = multiply(4, 6);
+echo "Multiplication of 4 and 6 is: $result\n";
+?>

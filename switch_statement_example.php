936a620e9c507dbde408787dcaee6f4fb424a018
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-15 20:43:43 +0530

    Add switch statement example in PHP

---
+++ switch_statement_example.php
@@ -0,0 +1,26 @@
+<?php
+// Switch statement in PHP
+
+$day = "Tuesday";
+
+switch ($day) {
+    case "Monday":
+        echo "Today is Monday.\n";
+        break;
+    case "Tuesday":
+        echo "Today is Tuesday.\n";
+        break;
+    case "Wednesday":
+        echo "Today is Wednesday.\n";
+        break;
+    case "Thursday":
+        echo "Today is Thursday.\n";
+        break;
+    case "Friday":
+        echo "Today is Friday.\n";
+        break;
+    default:
+        echo "It's a weekend!\n";
+        break;
+}
+?>
